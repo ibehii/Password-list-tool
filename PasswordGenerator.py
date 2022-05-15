f31f02d8c58f863d080d5a19a1312e71ff491d6b
Ahoora <75671611+AhSiber@users.noreply.github.com>
2022-05-15 12:48:47 +0430

    Make erase section and re-execute

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -10,7 +10,7 @@
 #  ____________________________________________
 
 #import part
-from os import name , system
+from os import system ; from sys import platform 
 from colorama import Fore
 from random import choice
 
@@ -29,11 +29,17 @@ print(Fore.GREEN , '''____       _
 user_request = int(input(Fore.YELLOW + '⹃ Enter number of your choice -> '+ Fore.RESET))
 
 # clear screen
-os_name = name 
-if (os_name == "posix"):
-    system('clear')
-else:
-    system('cls')
+
+if (platform == "Windwos".lower()): 
+    system('cls') 
+elif (platform == "Linux".lower()): 
+    system('clear') 
+else: 
+    try: 
+        system('clear') 
+    except: 
+        pass 
+
 
 print(Fore.MAGENTA + ''' ____                                     _ 
 |  _ \ __ _ ___ _____      _____  _ __ __| |
@@ -44,110 +50,148 @@ print(Fore.MAGENTA + ''' ____                                     _
 
 # Generaiting with number
 if (user_request == 1):
-    # Quantifying 
-    numbers = ['1','2','3','4','5','6','7','8','9','0']
-    generated_passsword_list = []
-    generated_passsword = choice(numbers)
+    def Generaiting_numbers():
+        # Quantifying 
+        numbers = ['1','2','3','4','5','6','7','8','9','0']
+        generated_passsword_list = []
+        generated_passsword = choice(numbers)
 
-    number_password = int(input(Fore.YELLOW + 'How many passwords do i have to generate ? ' + Fore.RESET))
-    len_password = int(input(Fore.YELLOW + 'How many number must passwords have ? ' + Fore.RESET))
-    # this `for` is about number of password that must generated 
-    for t in range (number_password):
-        # this `for` is about number of passowrd's letter that must generated
-        for i in range(len_password - 1):
-            temp_number = choice(numbers)
-            generated_passsword = generated_passsword + temp_number
-        if(generated_passsword in generated_passsword_list):
-            number_password = number_password + 1
+        number_password = int(input(Fore.YELLOW + 'How many passwords do i have to generate ? ' + Fore.RESET))
+        len_password = int(input(Fore.YELLOW + 'How many number must passwords have ? ' + Fore.RESET))
+        # this `for` is about number of password that must generated 
+        for t in range (number_password):
+            # this `for` is about number of passowrd's letter that must generated
+            for i in range(len_password - 1):
+                temp_number = choice(numbers)
+                generated_passsword = generated_passsword + temp_number
+            if(generated_passsword in generated_passsword_list):
+                number_password = number_password + 1
+            else:
+                generated_passsword_list.append(generated_passsword)
+                generated_passsword = choice(numbers)
+
+        # Add generated password to file 
+        file = open('passwords.txt', 'w')
+        generated_passswords = '\n'.join(generated_passsword_list)
+        file.write(generated_passswords)
+        file.close()
+        print(Fore.GREEN + 'Done !'+ Fore.RESET)
+        again = str(input('You want to make numbers again? [yes:no] : '))
+
+        if again == 'yes'.lower(): 
+            Generaiting_numbers() 
         else:
-            generated_passsword_list.append(generated_passsword)
-            generated_passsword = choice(numbers)
+            pass
 
-    # Add generated password to file 
-    file = open('passwords.txt', 'w')
-    generated_passswords = '\n'.join(generated_passsword_list)
-    file.write(generated_passswords)
-    file.close()
-    print(Fore.GREEN + 'Done !'+ Fore.RESET)
+    Generaiting_numbers() 
     
     
 # Generaiting with letters
 elif (user_request == 2):
+    def tolls(): 
 
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-               'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-    generated_passsword_list = []
-    generated_passsword = choice(letters)
-    
-    number_password = int(input(Fore.YELLOW + 'How many passwords do i have to generate ? ' + Fore.RESET))
-    len_password = int(input(Fore.YELLOW + 'How many letter must passwords have ? ' + Fore.RESET))
+        letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        generated_passsword_list = []
+        generated_passsword = choice(letters)
+        
+        number_password = int(input(Fore.YELLOW + 'How many passwords do i have to generate ? ' + Fore.RESET))
+        len_password = int(input(Fore.YELLOW + 'How many letter must passwords have ? ' + Fore.RESET))
 
-    # this `for` is about number of password that must generated
-    for t in range(number_password):
-        # this `for` is about number of passowrd's letter that must generated
-        for i in range(len_password - 1):
-            temp_letter = choice(letters)
-            generated_passsword = generated_passsword + temp_letter
-        if(generated_passsword in generated_passsword_list):
-            number_password = number_password + 1
+        # this `for` is about number of password that must generated
+        for t in range(number_password):
+            # this `for` is about number of passowrd's letter that must generated
+            for i in range(len_password - 1):
+                temp_letter = choice(letters)
+                generated_passsword = generated_passsword + temp_letter
+            if(generated_passsword in generated_passsword_list):
+                number_password = number_password + 1
+            else:
+                generated_passsword_list.append(generated_passsword)
+                generated_passsword = choice(letters)
+
+        file = open('passwords.txt', 'w')
+        generated_passswords = '\n'.join(generated_passsword_list)
+        file.write(generated_passswords)
+        file.close()
+        print(Fore.GREEN + 'Done !' + Fore.RESET)
+        
+        again = str(input('You want to make numbers again? [yes:no] : '))
+
+        if again == "yes".lower(): 
+            tolls()
         else:
-            generated_passsword_list.append(generated_passsword)
-            generated_passsword = choice(letters)
+            pass
+    tolls() 
 
-    file = open('passwords.txt', 'w')
-    generated_passswords = '\n'.join(generated_passsword_list)
-    file.write(generated_passswords)
-    file.close()
-    print(Fore.GREEN + 'Done !' + Fore.RESET)
+
 # generating with number and letter
 elif(user_request == 3):
-    numbers_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                       'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                       '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
-    generated_passsword_list = []
-    generated_passsword = choice(numbers_letters)
+    def sery(): 
+        numbers_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+        generated_passsword_list = []
+        generated_passsword = choice(numbers_letters)
 
-    number_password = int(input(Fore.YELLOW + 'How many passwords do i have to generate ? ' + Fore.RESET))
-    len_password = int(input(Fore.YELLOW + 'How many letter must passwords have ? ' + Fore.RESET))
+        number_password = int(input(Fore.YELLOW + 'How many passwords do i have to generate ? ' + Fore.RESET))
+        len_password = int(input(Fore.YELLOW + 'How many letter must passwords have ? ' + Fore.RESET))
 
-    for t in range (number_password):
-        for i in range(len_password - 1):
-            temp_letter_number = choice(numbers_letters)
-            generated_passsword = generated_passsword + temp_letter_number
-        if(generated_passsword in generated_passsword_list):
-            number_password = number_password + 1
+        for t in range (number_password):
+            for i in range(len_password - 1):
+                temp_letter_number = choice(numbers_letters)
+                generated_passsword = generated_passsword + temp_letter_number
+            if(generated_passsword in generated_passsword_list):
+                number_password = number_password + 1
+            else:
+                generated_passsword_list.append(generated_passsword)
+                generated_passsword = choice(numbers_letters)
+        
+        file = open('passwords.txt', 'w')
+        generated_passswords = '\n'.join(generated_passsword_list)
+        file.write(generated_passswords)
+        file.close()
+        print(Fore.GREEN + 'Done !' + Fore.RESET)
+        again = str(input('You want to make numbers again? [yes:no] : '))
+
+        if again == "yes".lower(): 
+            sery()
         else:
-            generated_passsword_list.append(generated_passsword)
-            generated_passsword = choice(numbers_letters)
-    
-    file = open('passwords.txt', 'w')
-    generated_passswords = '\n'.join(generated_passsword_list)
-    file.write(generated_passswords)
-    file.close()
-    print(Fore.GREEN + 'Done !' + Fore.RESET)
+            pass
+    sery() 
 
 # sort password list
 elif(user_request == 4):
-    file_name = input(Fore.YELLOW + 'Please, enter name of file (example : passwords.txt)→ ' + Fore.RESET)
-    file = open(file_name, 'r')
-    file_content = list(file.read())
-    file.close()
-    temp_password = ''
-    password_list = []
 
-    for i in file_content:
-        if i != '\n':
-            temp_password = temp_password + i
+    def fris():
+        file_name = input(Fore.YELLOW + 'Please, enter name of file (example : passwords.txt)→ ' + Fore.RESET)
+        file = open(file_name, 'r')
+        file_content = list(file.read())
+        file.close()
+        temp_password = ''
+        password_list = []
+
+        for i in file_content:
+            if i != '\n':
+                temp_password = temp_password + i
+            else:
+                password_list.append(temp_password)
+                temp_password = ''
+        password_list.sort()
+        str_password = '\n'.join(password_list)
+
+        file = open(file_name, 'w')
+        file.write(str_password)
+        file.close()
+        print(Fore.GREEN + 'Done !' + Fore.RESET)
+
+        again = str(input('You want to make numbers again? [yes:no] : '))
+
+        if again == "yes".lower(): 
+            fris()
         else:
-            password_list.append(temp_password)
-            temp_password = ''
-    password_list.sort()
-    str_password = '\n'.join(password_list)
-
-    file = open(file_name, 'w')
-    file.write(str_password)
-    file.close()
-    print(Fore.GREEN + 'Done !' + Fore.RESET)
+            pass
+    fris()
 
 else:
     print(Fore.RED + 'Something went wrong please try again !')
